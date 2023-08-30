9daf3c10e3dc3ad267f8bedd43175cbea47924a2
AbdulRahmanPro <progammerabood@gmail.com>
2023-08-31 00:04:12 +0300

    work start

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import LoginCard from "./components/loginCard";
+import './css/App.css';
+
+export default function App() {
+  return (
+    <div>
+      <LoginCard />
+    </div>
+  );
+}
---
+++ client/src/components/loginCard.js
@@ -0,0 +1,14 @@
+export default function loginCard(){
+    return (
+        <section className="Section_login">
+            <div className="title">
+              <h1>Login</h1>
+              <p>Glad you’re back.!</p>
+            </div>
+            <form>
+              <input></input>
+              <input></input>
+            </form>
+        </section>
+      )
+    }

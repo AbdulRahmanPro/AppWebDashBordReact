ef238e41c211be4ce5df2565ae74317be7ed5175
AbdulRahmanPro <progammerabood@gmail.com>
2023-09-01 01:10:36 +0300

    The login page and account creation have been completed, but modifications are required from the JWT side of the server

--- client/src/components/loginCard.js
+++ client/src/components/loginCard.js
@@ -1,9 +1,26 @@
-import React from 'react'; // Make sure to import React
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faEye } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function LoginCard() {
+  const [valueform,setvalueform] = useState({email:"",password:""})
+  const handleSubmit = (e)=>{
+    const {name , value} =  e.target
+    setvalueform({...valueform,[name]:value})
+  }
+  const Submit = (e) => {
+    e.preventDefault();
+
+    axios.post('http://localhost:3001/api/login', valueform)
+        .then((res) => {
+            console.log(res.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
   return (
     <div className='background_login w-screen h-screen '>
       <div className='flex flex-row-reverse justify-between items-center container'>
@@ -13,8 +30,8 @@ export default function LoginCard() {
           <p>Glad you’re back.!</p>
         </div>
         <form className="form_inputs space-y-5">
-          <input className="form_input" placeholder="Username" />
-          <input className="form_input" type="password" placeholder="Password" />
+          <input className="form_input" name='email' placeholder="Email" />
+          <input className="form_input" name="password" type="password" placeholder="Password" />
         </form>
         <div className="box_but_login flex flex-col justify-center gap-5 mt-6">
           <button className="but_login" type="submit">
@@ -22,7 +39,7 @@ export default function LoginCard() {
           </button>
           <p className="text-center">Forgot password ?</p>
         </div>
-        <div className="flex space-x-5 items-center justify-center mt-5">
+        <div className="flex space-x-5 items-center justify-center mt-5" onSubmit={Submit}>
           <img src={require("./icon/devicon_google.png")} alt="Google" />
           <img src={require("./icon/logos_facebook.png")} alt="Facebook" />
           <img src={require("./icon/bi_github.png")} alt="GitHub" />
--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,37 +1,57 @@
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-export default function register(props) {
+export default function Register(props) {
+    const [valueform, setvalueform] = useState({ email: '', username: '', password: '', name: '' });
+
+    const handleInput = (e) => {
+        const { name, value } = e.target;
+        setvalueform({ ...valueform, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post('http://localhost:3001/api/register', valueform)
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
     return (
-        <div className="background_register w-screen h-screen  "   >
-            <div className='container flex flex-row-reverse justify-between items-center '>
-            <section className="Section_register float-right">
-                <div className="title mb-4">
-                    <h1 className="text-4xl mx-auto my-auto">Signup</h1>
-                    <p>Just some details to get you in.!</p>
+        <div className="background_register w-screen h-screen">
+            <div className='container flex flex-row-reverse justify-between items-center'>
+                <section className="Section_register float-right">
+                    <div className="title mb-4">
+                        <h1 className="text-4xl mx-auto my-auto">Signup</h1>
+                        <p>Just some details to get you in.!</p>
+                    </div>
+                    <form className="form_inputs space-y-5" onSubmit={handleSubmit}>
+                        <input className="form_input" onChange={handleInput} value={valueform.name} name='name' type="text" placeholder="Name" />
+                        <input className="form_input" onChange={handleInput} value={valueform.username} name='username' placeholder="Username" />
+                        <input className="form_input" onChange={handleInput} value={valueform.email} name='email' placeholder="Email" />
+                        <input className="form_input" onChange={handleInput} value={valueform.password} name='password' type="password" placeholder="Password" />
+                        <div className="box_but_login flex flex-col justify-center gap-5 mt-6">
+                            <button className="but_singup" type="submit">
+                                Signup
+                            </button>
+                        </div>
+                    </form>
+                    <div className="flex space-x-5 items-center justify-center mt-5">
+                        <img src={require("./icon/devicon_google.png")} alt="Google" />
+                        <img src={require("./icon/logos_facebook.png")} alt="Facebook" />
+                        <img src={require("./icon/bi_github.png")} alt="GitHub" />
+                    </div>
+                    <Link to="/login" className=' text-center mt-10'>Already Registered? Login</Link>
+                </section>
+                <div>
+                    <h1 className="text-8xl text-center">Roll the Carpet.!</h1>
                 </div>
-                <form className="form_inputs space-y-5">
-                    <input className="form_input" placeholder="Username" />
-                    <input className="form_input" placeholder="Email" />
-                    <input className="form_input" type="password" placeholder="Password" />
-                    <input className="form_input" type="password" placeholder="Confirm Password" />
-                </form>
-                <div className="box_but_login flex flex-col justify-center gap-5 mt-6">
-                    <button className="but_singup" type="submit">
-                        Signup
-                    </button>
-                </div>
-                <div className="flex space-x-5 items-center justify-center mt-5">
-                    <img src={require("./icon/devicon_google.png")} alt="Google" />
-                    <img src={require("./icon/logos_facebook.png")} alt="Facebook" />
-                    <img src={require("./icon/bi_github.png")} alt="GitHub" />
-                </div>
-                <Link to="/login" className=' text-center mt-10'>Already Registered? Login </Link>
-            </section>
-            <div >
-                <h1 className=" text-8xl text-center  ">Roll the Carpet.!</h1>
-            </div>
             </div>
         </div>
-
-    )
+    );
 }

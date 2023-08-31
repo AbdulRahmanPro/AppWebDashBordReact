8a85a7757fab15fe2b193e597770bca5c52f6def
AbdulRahmanPro <progammerabood@gmail.com>
2023-08-31 10:14:47 +0300

    Preparing a library fontawesome

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import LoginCard from "./components/loginCard";
+import Content from "./components/content";
 import './css/App.css';
+import "./css/login.css"
 
 export default function App() {
   return (
-    <div>
+    <div className=" h-screen-mix flex flex-row-reverse gap-14 justify-center items-center ">
       <LoginCard />
+      <Content />
     </div>
   );
 }
---
+++ client/src/components/content.js
@@ -0,0 +1,8 @@
+
+export default function content(){
+    return(
+        <h1 className="text-8xl">
+            Welcome Back .!
+        </h1>
+    )
+}
